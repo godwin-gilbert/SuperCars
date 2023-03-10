ceb4663c1e02fc974454f1858c0ad369b388e0ba
godwin-gilbert <117091940+godwin-gilbert@users.noreply.github.com>
2023-03-10 11:23:47 +0530

    Add files via upload

---
+++ src/assets/dummy-data/booking-cars.js
@@ -0,0 +1,92 @@
+import car01 from "../images/car-01.png";
+import car02 from "../images/car-02.png";
+import car03 from "../images/car-03.png";
+import car04 from "../images/car-04.png";
+import car05 from "../images/car-05.png";
+import car06 from "../images/car-06.png";
+
+const bookingCars = [
+  {
+    id: "01",
+    carName: "Porshe 718 Cayman S",
+    category: "Audi",
+    type: "Manual",
+    groupSize: 4,
+    rentPrice: "14,129",
+    imgUrl: car01,
+  },
+  {
+    id: "02",
+    carName: "Porshe 718 Cayman S",
+    category: "Toyota",
+    type: "Manual",
+    groupSize: 4,
+    rentPrice: "13,209",
+    imgUrl: car02,
+  },
+  {
+    id: "03",
+    carName: "Porshe 718 Cayman S",
+    category: "Bmw",
+    type: "Manual",
+    groupSize: 4,
+    rentPrice: "17,109",
+    imgUrl: car03,
+  },
+  {
+    id: "04",
+    carName: "Porshe 718 Cayman S",
+    category: "Coupe",
+    type: "Manual",
+    groupSize: 4,
+    rentPrice: "20,012",
+    imgUrl: car04,
+  },
+  {
+    id: "05",
+    carName: "Porshe 718 Cayman S",
+    category: "Bmw",
+    type: "Manual",
+    groupSize: 4,
+    rentPrice: "15,409",
+    imgUrl: car05,
+  },
+  {
+    id: "06",
+    carName: "Porshe 718 Cayman S",
+    category: "Toyota",
+    type: "Manual",
+    groupSize: 4,
+    rentPrice: "14,129",
+    imgUrl: car06,
+  },
+  {
+    id: "07",
+    carName: "Porshe 718 Cayman S",
+    category: "Audi",
+    type: "Manual",
+    groupSize: 4,
+    rentPrice: "13,001",
+    imgUrl: car01,
+  },
+  {
+    id: "08",
+    carName: "Porshe 718 Cayman S",
+    category: "Toyota",
+    type: "Manual",
+    groupSize: 4,
+    rentPrice: "11,009",
+    imgUrl: car02,
+  },
+  {
+    id: "09",
+    carName: "Porshe 718 Cayman S",
+    category: "Bmw",
+    type: "Manual",
+    groupSize: 4,
+    rentPrice: "10,009",
+    imgUrl: car03,
+  },
+];
+
+export default bookingCars;
